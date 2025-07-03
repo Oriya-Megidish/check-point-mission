a32b7cf6aa1c22bc4df0e3ad01cb216476319954
Oriya-Megidish <149678492+Oriya-Megidish@users.noreply.github.com>
2025-07-03 23:11:23 +0300

    Update variables.tf

--- infrastructure/modules/iam_role/variables.tf
+++ infrastructure/modules/iam_role/variables.tf
@@ -11,9 +11,11 @@ variable "assume_role_service" {
 variable "permissions" {
   description = "List of permission keys to assign to this role. Supported: s3_read, s3_write, sqs_send, sqs_receive, sqs_delete, ssm_get, ecr_pull, cloudwatch_write"
   type = list(string)
+  default = []
 }
 
 variable "resources" {
   description = "List of resource ARNs to which the permissions apply"
   type = list(string)
+  default = []
 }
