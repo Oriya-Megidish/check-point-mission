b34acff26d5b0a6477f72a2b0c86be81887875b9
Oriya-Megidish <149678492+Oriya-Megidish@users.noreply.github.com>
2025-07-03 23:58:58 +0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -30,7 +30,7 @@ module "s3_kms" {
   description     = "KMS key for S3 encryption"
   bucket_name     = local.s3_bucket_name
   admin_role_arn  = var.admin_role_arn
-  extra_key_users = [aws_iam_role.pre_ecs_rest_service_task_role.role_arn, aws_iam_role.pre_ecs_sql_listener_task_role.role_arn]
+  extra_key_users = [aws_iam_role.pre_ecs_rest_service_task_role.arn, aws_iam_role.pre_ecs_sql_listener_task_role.arn]
 }
 
 module "sqs_kms" {
@@ -39,7 +39,7 @@ module "sqs_kms" {
   description     = "KMS key for SQS encryption"
   sqs_queue_arn   = "arn:aws:sqs:${var.aws_region}:${var.account_id}:${local.sqs_queue_name}"
   admin_role_arn  = var.admin_role_arn
-  extra_key_users = [aws_iam_role.pre_ecs_rest_service_task_role.role_arn, aws_iam_role.pre_ecs_sql_listener_task_role.role_arn]
+  extra_key_users = [aws_iam_role.pre_ecs_rest_service_task_role.arn, aws_iam_role.pre_ecs_sql_listener_task_role.arn]
 }
 
 resource "aws_ssm_parameter" "my_parameter" {
