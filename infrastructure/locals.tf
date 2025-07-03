9ca0172993bad2d26f1a6522264f1b25e41114e7
Oriya-Megidish <149678492+Oriya-Megidish@users.noreply.github.com>
2025-07-03 13:25:25 +0300

    Update locals.tf

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -3,7 +3,7 @@ locals {
   sqs_queue_name = "${var.owner}-sqs-queue"
   cluster_name = "${var.owner}-cluster"
   parameter_path = "/${var.owner}/token"
-  rest_repo_name = ""rest_service"
+  rest_repo_name = "rest_service"
   rest_version = "latest"
   sql_listener_repo_name = "sql_listener"
   sql_listener_version = "latest"
