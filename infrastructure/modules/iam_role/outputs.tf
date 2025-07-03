ace456f6bb23061f59c2f4674647a3776498a28a
Oriya-Megidish <149678492+Oriya-Megidish@users.noreply.github.com>
2025-07-03 23:58:04 +0300

    Update outputs.tf

--- infrastructure/modules/iam_role/outputs.tf
+++ infrastructure/modules/iam_role/outputs.tf
@@ -1,9 +1,1 @@
-﻿output "role_arn" {
-  description = "ARN of the IAM Role"
-  value       = aws_iam_role.this.arn
-}
 
-output "role_name" {
-  description = "Name of the IAM Role"
-  value       = aws_iam_role.this.name
-}
