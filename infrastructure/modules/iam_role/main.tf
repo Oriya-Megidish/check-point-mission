3cfd910d9831df7b4f80386c24344335e1d81b1b
Oriya-Megidish <149678492+Oriya-Megidish@users.noreply.github.com>
2025-07-03 23:31:36 +0300

    Update main.tf

--- infrastructure/modules/iam_role/main.tf
+++ infrastructure/modules/iam_role/main.tf
@@ -26,11 +26,11 @@ data "aws_iam_policy_document" "role_policy" {
 resource "aws_iam_policy" "this" {
   count = length(var.resources) > 0 ? 1 : 0
   name   = "${var.role_name}-policy"
-  policy = data.aws_iam_policy_document.role_policy.json
+  policy = data.aws_iam_policy_document.role_policy[0].json
 }
 
 resource "aws_iam_role_policy_attachment" "this" {
   count = length(var.resources) > 0 ? 1 : 0
   role       = aws_iam_role.this.name
-  policy_arn = aws_iam_policy.this.arn
+  policy_arn = aws_iam_policy.this[0].arn
 }
