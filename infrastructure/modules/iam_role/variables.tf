7bcaa542c0256c78ec09166a4aec9534536af266
Oriya-Megidish <149678492+Oriya-Megidish@users.noreply.github.com>
2025-07-03 23:51:13 +0300

    Update variables.tf

--- infrastructure/modules/iam_role/variables.tf
+++ infrastructure/modules/iam_role/variables.tf
@@ -3,11 +3,6 @@
   type = string
 }
 
-variable "assume_role_service" {
-  description = "The AWS service principal that can assume this role, e.g. ecs-tasks.amazonaws.com"
-  type = string
-}
-
 variable "permissions" {
   description = "List of permission keys to assign to this role. Supported: s3_read, s3_write, sqs_send, sqs_receive, sqs_delete, ssm_get, ecr_pull, cloudwatch_write"
   type = list(string)
