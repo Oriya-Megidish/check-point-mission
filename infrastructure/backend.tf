f1b28325a96e79d6c084f3e45c9489e972777c9b
Oriya-Megidish <149678492+Oriya-Megidish@users.noreply.github.com>
2025-08-17 00:07:59 +0300

    Update backend.tf

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -2,7 +2,7 @@
   backend "s3" {
     bucket = "terraform-state-bucket-oriya-hen-1"   
     key = "project/terraform.tfstate"  
-    region = "us-east-2"                   
+    region = "us-east-1"                   
     dynamodb_table = "oriya-terraform-lock-table-1"        
     encrypt = true
     }
