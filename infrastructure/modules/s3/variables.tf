2db4ee7a86e32b81a71b48565d15e3e276a6bd67
Oriya-Megidish <149678492+Oriya-Megidish@users.noreply.github.com>
2025-08-16 23:48:05 +0300

    Update variables.tf

--- infrastructure/modules/s3/variables.tf
+++ infrastructure/modules/s3/variables.tf
@@ -14,11 +14,6 @@ variable "force_destroy" {
   description = "Whether to allow deletion of non empty bucket"
 }
 
-variable "admin_role_arn" {
-  description = "ARN of the admin IAM role with full access to the bucket"
-  type        = string
-}
-
 variable "role_permissions" {
   description = "Map of IAM Role ARN to list of permissions (read, write)"
   type        = map(list(string))
